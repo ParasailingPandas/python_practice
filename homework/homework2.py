89acc78273cc8cf6f1e8ab7d7c328abb0c241490
melmatt7 <mel.matt007@gmail.com>
2020-05-07 15:48:56 -0700

    homework2

---
+++ homework/homework2.py
@@ -0,0 +1,12 @@
+# Given two lists in ascending order, return a combined list also in ascending order
+
+# Examples
+# - mergeStortedLists([0,2,4],[1,3,5]) -> [0,1,2,3,4,5]
+# - mergeStortedLists([-3,-2,0],[-10,0,5]) -> [-10,-3,-2,0,0,5]
+
+def mergeStortedLists(list1, list2):
+    # code goes here
+    return # mergedList
+
+print(mergeStortedLists([0,2,4],[1,3,5]))
+print(mergeStortedLists([-3,-2,0],[-10,0,5]))
