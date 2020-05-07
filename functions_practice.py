a4ec207e6c5310e9d6e6366847334a95f07d674a
melmatt7 <mel.matt007@gmail.com>
2020-05-07 15:29:19 -0700

    Functions practice

---
+++ functions_practice.py
@@ -0,0 +1,16 @@
+# Write a function that given a number as a string returns True 
+# if it is a Palindrome and returns False if not a Palindrome
+
+# Examples:
+# - isPalindrome("121") returns True
+# - isPalindrome("001") returns False
+# - isPalindrome("2") returns True
+
+
+def isPalindrome(num):
+    # write your code here
+    return True # or False
+
+isPalindrome("121")
+isPalindrome("001")
+isPalindrome("2")
