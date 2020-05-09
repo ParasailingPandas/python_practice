863225f8a14088a7b980baaa9774ebfa4df4a47d
melmatt7 <mel.matt007@gmail.com>
2020-05-08 17:22:46 -0700

    homework4

---
+++ homework/homework4.py
@@ -0,0 +1,14 @@
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+# Examples
+# - hotSinglesInYourArea([0,0,1,1,4,4,5,7,6,7,5]) -> returns 6
+# - hotSinglesInYourArea([0,56,78,43,56,43]) -> returns 78
+# - hotSinglesInYourArea([0,9,0,9]) -> returns "None"
+
+def hotSinglesInYourArea(list1):
+    # code here
+    return # a number
+
+print(hotSinglesInYourArea([0,0,1,1,4,4,5,7,6,7,5]))
+print(hotSinglesInYourArea([0,56,78,43,56,43]))
+print(hotSinglesInYourArea([0,9,0,9]))
